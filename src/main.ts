8e0c8488f2bb22622ce76150b04818b2d58aae9c
AndreyBest <andrey255@live.com>
2020-01-03 22:09:25 +0200

    Remove useless comments

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,11 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
   app.quit();
 }
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow: BrowserWindow | null;
 
 let server: WebServer;
 
 function createWindow(): void {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 600,
     maxWidth: 900,
@@ -29,14 +26,9 @@ function createWindow(): void {
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/../public/menu/index.html`);
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 }
@@ -64,9 +56,6 @@ function createPresentationWindow(htmlLink: string): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
   createWindow();
 });
@@ -91,7 +80,6 @@ ipcMain.on('open-file', async () => {
   }
 });
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
@@ -107,6 +95,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
